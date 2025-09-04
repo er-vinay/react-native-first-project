906299bbd4cea2942510dc15545bb5f2570fdd89
Vinay Kumar <vinay.kumar@agrimfincap.com>
2025-09-04 19:28:28 +0530

    react native project code moved to git

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize : 30}}>React Native Apps</Text>
+      <Text style={{fontSize : 30}}>React Native Apps</Text>
+      <Text style={{fontSize : 30}}>React Native Apps</Text>
+      <Text style={{fontSize : 30}}>React Native Apps</Text>
+      <Button title='Press Here' />
+    </View>
+  );
+};
+
+export default App;
